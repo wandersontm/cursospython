0e78ff2808a7644ef54cb2a919bb135b5ea47cfa
wandersontm <wandersontmcontato@gmail.com>
2026-08-02 20:54:05 -0300

    add aula 7

---
+++ aula 7.py
@@ -0,0 +1,24 @@
+#ordem de precedência em python:
+#primeiro se executa:
+#1º = ()
+#2º = **
+#3º = * / // %
+#4ª = + -
+
+
+# while True:
+#     try:
+#         n1 = int(input('digite um numero: '))
+#         break
+#     except ValueError:
+#         print('por favor digite um numero inteiro')
+#
+# while True:
+#     try:
+#         n2 = int(input('digite outro numero: '))
+#         break
+#     except ValueError:
+#         print('por favor digite um numero inteiro')
+#
+# s = n1 + n2
+# print(f'A soma é', s)
