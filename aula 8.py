8f03ecea5a0b4cf55b9fc768e5150e145f2ffc54
wandersontm <wandersontmcontato@gmail.com>
2026-08-17 12:47:19 -0300

    atualizando aula 8

--- aula 8.py
+++ aula 8.py
@@ -1,2 +1,53 @@
-import emoji
-print(emoji.emojize('Olá gay. :earth_americas:', language='alias'))
+# import emoji
+# print(emoji.emojize('Olá gay.:earth_americas:', language='alias'))
+
+#função ceil de import de math faz arrendodamento de número para cima
+# floor arredonda para baixo
+#trunc elimina a parte após a vírgula sem nenhum arrendamento
+#pow potência de um número
+#sqrt calcula uma raiz quadrada
+#factorial calcula um fatorial de um número
+
+# import math
+# # from math import sqrt << quando quiser importar somente uma função.
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print(f'A raiz de {num} é igual a {math.trunc(raiz)}')
+
+# from math import sqrt, floor
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print(f'A raiz de {num} é igual a {floor(raiz)}')
+
+# import random
+# num = random.random()
+# #randomiza um número entre 0 e 1
+# print(num)
+
+# num_ran = random.randint(1, 100)
+# #randomiza um número em um intervalo específico
+# print(num_ran)
+
+# desafio 16
+# crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+# ex: digite um número: 6.127
+# o número 6.127 tem a parte inteira 6.
+
+# desafio 17:
+# faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo,
+# calcule e mostre o comprimento da hipotenusa.
+
+# desafio 18:
+# faça um programa que leia um angulo qualquer e mostre na tela o valor do seno,
+# cosseno e tangente desse angulo.
+
+# desafio 19:
+# um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+# faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+# desafio 20:
+# o prof quer sortear a ordem de apresentação de trabalhos dos alunos.
+# faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+# desafio 21:
+# faça um programa em python que abra e reproduza um audio de um arquivo mp3.
