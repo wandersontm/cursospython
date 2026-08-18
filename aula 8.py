8d9056f1c15da4a984de8bd3e4331d484e9559ca
wandersontm <wandersontmcontato@gmail.com>
2026-08-17 22:18:01 -0300

    atualizando os desafios da aula 8

--- aula 8.py
+++ aula 8.py
@@ -1,7 +1,7 @@
 # import emoji
 # print(emoji.emojize('Olá gay.:earth_americas:', language='alias'))
 
-#função ceil de import de math faz arrendodamento de número para cima
+#função ceil de import de math faz arrendondamento de número para cima
 # floor arredonda para baixo
 #trunc elimina a parte após a vírgula sem nenhum arrendamento
 #pow potência de um número
@@ -28,26 +28,65 @@
 # #randomiza um número em um intervalo específico
 # print(num_ran)
 
-# desafio 16
+
+# desafio 16:
 # crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
 # ex: digite um número: 6.127
 # o número 6.127 tem a parte inteira 6.
+#
+# import math
+# num = float(input('digite um numero:'))
+# print(f' O número {num} tem a parte inteira {math.trunc(num)}')
+
 
 # desafio 17:
 # faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo,
 # calcule e mostre o comprimento da hipotenusa.
 
+# import math
+# cateto_a = int(input("Digite o valor de cateto a: "))
+# teteto_b = int(input("Digite o valor de cateto b: "))
+# print(math.hypot(cateto_a,teteto_b))
+
+
 # desafio 18:
 # faça um programa que leia um angulo qualquer e mostre na tela o valor do seno,
 # cosseno e tangente desse angulo.
+
+# import math
+# num = float(input(f'digite um numero: '))
+# graus_num = math.radians(num)
+# print(f'o seno de {num}º é {math.sin(graus_num):.2f}, seu cosseno é {math.cos(graus_num):.2f} e sua tangente é {math.tan(graus_num):.2f}')
+
 
 # desafio 19:
 # um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 # faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 
+# import math
+# import random
+# alunos = ("Aline","Joice","Jao","Gay")
+# aluno_escolhido = random.choice(alunos)
+# if aluno_escolhido in ["Aline","Joice"]:
+#     print(f'Entre os alunos da classe, o escolhido para a tarefa foi a {aluno_escolhido}')
+# else:
+#     print(f'Entre os alunos da classe, o escolhido para a tarefa foi o {aluno_escolhido}')
+
+
 # desafio 20:
 # o prof quer sortear a ordem de apresentação de trabalhos dos alunos.
 # faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+
+# import random
+# from random import shuffle
+#
+# alunos = ['Aline','Joice','Jao','Gay']
+# aluno_escolhido = random.choice(alunos)
+#
+# print(f' Os alunos da turma são {', '.join(alunos)}.')
+# shuffle(alunos)
+# print(f' A sequencia de apresentação será: {', '.join(alunos)}')
+
 
 # desafio 21:
 # faça um programa em python que abra e reproduza um audio de um arquivo mp3.
