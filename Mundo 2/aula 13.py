cef0061eb739fd0807b77468e3f3ab4c994004ad
wandersontm <wandersontmcontato@gmail.com>
2026-08-31 23:35:14 -0300

    desafios da aula 13

--- Mundo 2/aula 13.py
+++ Mundo 2/aula 13.py
@@ -23,16 +23,46 @@
 # faça um programa ( la ele ) que mostre na tela uma contagem regressiva para o estouro de fogos
 # de artifício indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 
+# from cores import *
+# from time import sleep
+# for c in range(10, -1, -1):
+#     print(f'{c}...')
+#     sleep(1)
+# print(f'{VERMELHO_BOLD}BOOM!!!!{LIMPA}')
+# print('Feliz Ano Novo!!')
+# sleep(1)
+# print(f'{VERMELHO_BOLD}BOOM!!!!{LIMPA}')
+# print('"fogos estourando..."')
+
 
 
 # desafio 47
 # faça um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
+# for c in range(1,51):
+#     if c == 50:
+#         print(f'{c}!',end='')
+#     elif c%2==0:
+#         print(c, '-',end=' ')
+
+# for c in range(2,51,2):
+#     print(c, end=' ')
+# print('ACABOU!!!')
 
 
 # desafio 48
 # faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que
 # se encontram no intervalo de 1 até 500.
+
+# soma = 0
+# repeticao = 0
+# for c in range(1, 501, 2):
+#
+#     if c % 3 == 0:
+#         soma += c
+#         repeticao += 1
+#
+# print(f'A soma dos {repeticao} valores é {soma}')
 
 
 
@@ -40,11 +70,32 @@
 # refaça o desafio 009 mostrando a tabuada de um número que o usuário escolher, so que agora utilizando
 # um laço for.
 
+# numero = int(input(f'Digite um número para mostrar sua tabuada: '))
+# for c in range(1,11):
+#     if c < 10:
+#         print(f'{numero} x {c} = {numero*c};')
+#     else:
+#         print(f'{numero} x {c} = {numero*c}.')
+
 
 
 # desafio 50
-# programa que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares.
+# programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
 # se o valor digitado for impar, desconsidere-o.
+
+# soma = 0
+# cont = 0
+# for c in range(1,7):
+#     n = int(input(f'Digite o {c}º número: '))
+#     if n % 2 == 0:
+#         soma += n
+#         cont += 1
+# if cont == 0:
+#     print(f'Você não informou nenhum número par, logo a soma dos pares é igual a zero!!!')
+# elif cont == 1:
+#     print(f'Você digitou {cont} número par e a soma é igual a {soma}!!!!')
+# else:
+#     print(f'Você digitou {cont} números pares e a soma é igual a {soma}!!!!')
 
 
 
@@ -52,10 +103,39 @@
 # programa que leia o primeiro termo e a razão de uma PA. no final,
 # mostre os 10 primeiros termos dessa progressão.
 
-
+# p1 = int(input('Digite o primeiro termo da PA: '))
+# razao = int(input(f'Digite a razão da PA: '))
+# decimo = p1 + 10 * razao
+# # for c in range(0,10):
+# #     if c == 9:
+# #         print(f'{p1 + razao * c}.')
+# #     else:
+# #         print(f'{p1 + razao * c} ->', end =' ')
+#
+# for c in range(p1, decimo, razao):
+#     print(c, end=' ')
 
 # desafio 52
 # faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+# from cores import *
+#
+# n = int(input('Digite um número: '))
+# con = 0
+#
+# for c in range(1,n+1):
+#     if n % c == 0:
+#         con += 1
+#         print(f'{VERMELHO}{c}{LIMPA}',end=' ')
+#
+#     elif n % c != 0:
+#         print(f'{c}',end=' ')
+# print()
+# print(f'O número {n} foi divisível {con} vezes!!!')
+# if con != 2:
+#     print(f'O número não é primo!!!')
+# elif con == 2:
+#     print(f'O número é primo!!!')
 
 
 
