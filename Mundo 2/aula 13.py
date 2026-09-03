4b605ba64a2e4d7f52cf1ab0848b2dcc11dc77a8
wandersontm <wandersontmcontato@gmail.com>
2026-09-02 21:09:11 -0300

    desafios da aula 13

--- Mundo 2/aula 13.py
+++ Mundo 2/aula 13.py
@@ -50,6 +50,7 @@
 # print('ACABOU!!!')
 
 
+
 # desafio 48
 # faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e que
 # se encontram no intervalo de 1 até 500.
@@ -115,6 +116,8 @@
 # for c in range(p1, decimo, razao):
 #     print(c, end=' ')
 
+
+
 # desafio 52
 # faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 
@@ -141,32 +144,32 @@
 
 # desafio 53
 # que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
-print('Vamos descobrir se uma frase é um palindrome')
-# frase = str(input(f'Digite uma frase: '))
-frase = ' Ola alo'
-texto = frase
-print(f'A frase : "{frase}".')
-frase = frase.lower()
-frase = ''.join(frase.split())
-n_letras = len(frase)
-palindrome = True
-for c in range(0, n_letras):
-    if c < n_letras // 2:
-        ultima = frase[n_letras - c - 1]
-        letra = frase[c]
-        # print(frase[c], end = ' ') #debugs
-        # print(ultima, end = ' ') #debugs
-        if letra != ultima:
-            palindrome = False
-            # print(palindrome) #debugs
-        # elif letra == ultima:
-            # print(True) #debugs
-
-if palindrome:
-    print(f'A frase "{texto}" é um Palindrome!!!')
-
-if not palindrome:
-    print(f'A frase "{texto}" não é um palindrome!!!')
+# print('Vamos descobrir se uma frase é um palindrome')
+# # frase = str(input(f'Digite uma frase: '))
+# frase = ' Ola alo'
+# texto = frase
+# print(f'A frase : "{frase}".')
+# frase = frase.lower()
+# frase = ''.join(frase.split())
+# n_letras = len(frase)
+# palindrome = True
+# for c in range(0, n_letras):
+#     if c < n_letras // 2:
+#         ultima = frase[n_letras - c - 1]
+#         letra = frase[c]
+#         # print(frase[c], end = ' ') #debugs
+#         # print(ultima, end = ' ') #debugs
+#         if letra != ultima:
+#             palindrome = False
+#             # print(palindrome) #debugs
+#         # elif letra == ultima:
+#             # print(True) #debugs
+#
+# if palindrome:
+#     print(f'A frase "{texto}" é um Palindrome!!!')
+#
+# if not palindrome:
+#     print(f'A frase "{texto}" não é um palindrome!!!')
 
 
 
@@ -174,10 +177,41 @@ if not palindrome:
 # que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram
 # a maioridade e quantas já são maiores.
 
+# from datetime import date
+# nascimento = 0
+# maior = 0
+# menor = 0
+# date = date.today().year
+# for c in range(0, 6):
+#     nascimento += int(input(f'Digite {c+1}ª data de nascimento: '))
+#     if date - nascimento >= 18 :
+#         maior += 1
+#     elif date - nascimento < 18 :
+#         menor += 1
+#
+# print(date - nascimento)
+# print(f'Maior de idade: {maior}!!')
+# print(f'Menor de idade: {menor}!!')
+
 
 
 # desafio 55
 # que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lido.
+
+# atual = 0
+# maior = 0
+# menor = 0
+# for c in range(1, 6):
+#     atual = int(input(f'Digite o {c}º peso: '))
+#     if c == 1:
+#         maior = atual
+#         menor = atual
+#     elif atual >= maior:
+#         maior = atual
+#     elif atual <= menor:
+#         menor = atual
+# print(f'O maior peso foi de {maior}Kg')
+# print(f'O menor peso foi de {menor}Kg')
 
 
 
@@ -186,3 +220,43 @@ if not palindrome:
 # a média de idade do grupo.
 # Qual é o nome do homem mais velho.
 # Quantas mulheres têm menos de 20 anos.
+
+# nome = 0
+# idade = 0
+# homem_velho = ''
+# maior = 0
+# dimenor = 0
+# sexo = ''
+# media = 0
+# contador = 0
+# listamulher = []
+# listahomem = []
+# for c in range(1, 5):
+#     nome = str(input(f'Digite o nome da {c}º pessoa: '))
+#     idade =int(input(f'Digite a idade da {c}º pessoa: '))
+#     sexo = str(input(f'Digite o sexo (f/m): '))
+#     media += idade
+#     contador += 1
+#
+#     if sexo in 'Ff':
+#         listamulher += [nome]
+#         if idade < 20:
+#             dimenor += 1
+#
+#     elif sexo == 'm':
+#         listahomem += [nome]
+#         if idade > maior:
+#             homem_velho = nome
+#             maior = idade
+#
+# print(listamulher)
+# print(listahomem)
+# print(f'''O nome do homem mais velho é {homem_velho} com {maior} anos.
+# a média da idade do grupo é {media / contador:.0f} anos.''')
+#
+# if dimenor > 1:
+#     print(f'a quantidade de mulheres com menos de 20 anos é {dimenor}.')
+# elif dimenor == 1:
+#     print(f' temos uma mulher com menos de 20 anos.')
+# elif dimenor == 0:
+#     print(f' Não temos mulheres com menos de 20 anos.')
---
+++ Mundo 2/aula 14.py
@@ -0,0 +1,71 @@
+from cores import *
+
+# maça = 0
+# while not maça == 3:
+#     print('Funciona?')
+#     for c in range(0, 3):
+#         maça +=1
+#         c -=1
+#         print(f'Contando maças {maça}')
+
+
+print('='*15,f'{VERMELHO}COMEÇANDO{LIMPA}','='*15 )
+
+# desafio 57
+#  que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. caso esteja errado peça a digitação
+# novamente até ter um valor correto.
+
+
+
+# desafio 58
+# melhore o jogo do desafio 028, o computador vai pensar em um numero entre 0 e 10. Só que agora o jogador vai tentar
+# adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
+
+
+# desafio 59
+# que leia dois valores e mostre um menu na tela:
+# [1] somar
+# [2] multiplicar
+# [3] maior
+# [4] novos números
+# [5] sair do programa
+#deverá realizar a operação solicitada em cada caso.
+
+
+
+# desafio 60
+# que leia um número qualquer e mostre seu fatorial.
+# ex: 5! = 5x4x3x2x1 = 120
+
+
+
+# desafio 61
+# refaça o desafio 51, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+
+
+# desafio 62
+# melhore o desafio 61 perguntando para o usuário se ele quer mostrar mais alguns termos
+# O programa encerra quando ele disser que quer mostrar 0 termos.
+
+
+
+# desafio 63
+# que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequência de Fibonacci.
+# ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
+
+
+
+# desafio 64
+# que leia varios numeros inteiros pelo teclado. o programa só vai parar quando o usuário digitar o valor 999.
+# que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
+# desconsiderando o flag.
+
+
+
+# desafio 65
+# que leia varios numeros inteiros pelo teclado. no final da execução, mostre a média entre todos os valores
+# e qual foi o maior e o menor valor lido.
+# o programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores
