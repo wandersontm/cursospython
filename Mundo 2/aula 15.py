35f6814fe11581035eff002f8fded54b0a006627
wandersontm <wandersontmcontato@gmail.com>
2026-09-08 17:00:39 -0300

    desafios da aula 15

---
+++ Mundo 2/aula 15.py
@@ -0,0 +1,56 @@
+# n = s = 0
+# while True:
+#     n = int(input('digite um numero: '))
+#     if n == 999:
+#         break
+#     s += n
+#print(f'A soma vale {s}.')
+
+
+from cores import *
+print('='*15,f'{VERMELHO}COMEÇANDO{LIMPA}','='*15 )
+
+
+#desafio 66
+# que leia vários números inteiros. Pare quando 999 for digitado.
+# quantos foram digitados e a soma entre eles.
+
+
+# desafio 67
+# mostre a tabuada de um número.
+# Pare quando um número negativo for inserido
+
+
+# desafio 68
+# jogue par ou impar com o computador.
+# Interromper quando o jogador perder
+# mostrando os valores do jogo atual, e o resultado
+# mostrando o total de vitórias.
+
+
+# desafio 69
+# leia idade e sexo de várias pessoas.
+# a cada entrada perguntar se quer parar ou continuar
+# no final mostrar quantas pessoas tem mais de 18 anos
+# quantos homens foram cadastrados.
+# Quantas mulheres tem menos de 20 anos.
+
+
+# desafio 70
+# leia o nome e preço de vários produtos
+# perguntar ao usuário se vai continuar
+# no final mostar o total gasto
+# quantos produtos custaram mais de 1000
+#qual o nome do produto mais barato
+
+
+# desafio 71
+# simule um caixa eletrônico
+# no início perguntar qual o valor a ser sacado
+# e informar quantas células de cada valor serão entregues
+# usar as notas de 1, 10, 20 e 50.
+
+
+print()
+print()
+print('='*15,f'{VERMELHO} FIM {LIMPA}','='*20)
