bd5708ea560bd439dbbe3c1cb7e5465129470978
wandersontm <wandersontmcontato@gmail.com>
2026-09-16 12:56:58 -0300

    da aula 17

--- Mundo 3/aula 17.py
+++ Mundo 3/aula 17.py
@@ -1,5 +1,6 @@
 from cores import *
 print('='*15,f'{VERMELHO}COMEÇANDO{LIMPA}','='*16 )
+print()
 
 # lista_num = [2, 5, 9, 1] # cria lista
 # lista_num[2] = 3 # substitui o elemento em [2], no caso o número '9' por 3.
@@ -17,16 +18,44 @@ print('='*15,f'{VERMELHO}COMEÇANDO{LIMPA}','='*16 )
 #    num.remove(4) # checa se o valor existe na lista antes de remover. Corrigindo o erro de não existe na lista.
 # print(lista_num)
 
-valores = []
-valores.append(5)
-valores.append(9)
-valores.append(4)
-
+# valores = list()
+# for cont in range(0,5):
+#     valores.append(int(input(f'Digite um valor: ')))
+#
 # for v in valores: # printa os valores conforme a formatação no print
-#     print(f'{v}...', end='')
+#      print(f'{v}...', end='')
+#
+# for c, v in enumerate(valores):
+#     print(f'Na posição {c} encontrei o valor {v}!')
 
-for c, v in enumerate(valores):
-    print(f'Na posição {c} encontrei o valor {v}!')
+# a = [2,3,8,7]
+# b = a # faz com que as listas sejam atualizadas juntas, mexeu b mexeu a.
+# b = a[:] # forma de b receber os elementos de a sem criar vinculo entre as listas.
+# b[2] = 8
+# print(a)
+# print(b)
+
+
+# Desafio 78
+# faça um programa que leia 5 valores numéricos e guarde os em uma lista.
+# no final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista
+
+
+
+# desafio 79
+# crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista
+# caso o número já exista lá dentro, ele não será adicionado.
+# No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+
+
+
+# desafio 80
+# crie um programa onde o usuário possa digitar cinco valores numéricos
+# e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()).
+# no final, mostre a lista ordenada na tela.
+
+
+
 
 
 
